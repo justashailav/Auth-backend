5e90bc9ecbd631b5d5c4ccaaef303c696ce9f086
Shailav <justashailav02@gmail.com>
2025-12-24 14:51:34 +0530

    Initial backend commit

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,9 +1,10 @@
 import express from "express"
 import { createProduct, getAllProducts } from "../controllers/productController.js"
+import upload from "../utils/multer.js";
 
 const router =express.Router()
 
-router.post("/create-product",createProduct);
+router.post("/create-product",upload.array("images", 5),createProduct);
 router.get("/getAllProducts",getAllProducts)
 
 export default router;
