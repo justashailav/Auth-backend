4ba9101d33b41c9df021a659eeed7a860c097885
Shailav <justashailav02@gmail.com>
2025-12-25 13:39:56 +0530

    Initial backend commit

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -2,7 +2,6 @@ import express from "express"
 import { addToCart, getCart } from "../controllers/cartController.js";
 import { isAuthenticated } from "../middleware/isAuthenciated.js";
 
-
 const router =express.Router()
 router.post("/add-cart",isAuthenticated,addToCart);
 router.get("/getCart",isAuthenticated,getCart)
