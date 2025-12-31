311ca6bff395f7839b086598d08ba589aadf739a
Shailav <justashailav02@gmail.com>
2025-12-31 14:31:05 +0530

    Initial commit

--- models/orderModel.js
+++ models/orderModel.js
@@ -66,14 +66,16 @@ const orderSchema = new mongoose.Schema(
 
     orderStatus: {
       type: String,
-      enum: [
-        "PLACED",
-        "CONFIRMED",
-        "SHIPPED",
-        "DELIVERED",
-        "CANCELLED",
-      ],
+      enum: ["PLACED", "CONFIRMED", "SHIPPED", "DELIVERED", "CANCELLED"],
       default: "PLACED",
+    },
+
+    razorpayOrderId: {
+      type: String,
+    },
+
+    razorpayPaymentId: {
+      type: String,
     },
   },
   { timestamps: true }
